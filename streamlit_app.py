af0587c333cc9175f30ea4857106d8b9d17c7271
ACDameron <ashley.dameron7@gmail.com>
2026-07-23 17:01:14 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
-
+import time
 
 st.title("Hello world!")
 
@@ -16,13 +15,31 @@ st.subheader("st.column")
 col1, col2 = st.columns(2)
 
 with col1:
-    x = st.slider("Choose an x value",1,10)
+    x = st.slider("Choose an x value",1,1000)
+
+progress_bar = col1.progress(0)
+
+for perc_completed in range (100):
+    time.sleep(0.05)
+    progress_bar.progress(perc_completed+1)
+
+col1.success("Value chosen successfully")
+
+
+
 with col2:
     st.write("The value of :orange[***x***] is",x)
+
+col2.metric(label="Temperature", value="80 °F", delta="5 °F")
 
 
 from numpy.random import default_rng as rng
 
 df = pd.DataFrame(rng(0).standard_normal((20, 3)), columns=["a", "b", "c"])
 
-st.area_chart(df)
+col3, col4 = st.columns(2)
+
+with col3:
+     st.area_chart(df)
+with col4:
+     st.bar_chart(df)
